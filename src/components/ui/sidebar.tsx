0f7008f27da3b716d4a38cedc11bea0df7720400
Andrew <andrew@zezwe-it.co.za>
2026-04-10 11:05:50 +0200

    fix: move TooltipTrigger outside of useRender to fix type error

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -518,7 +518,7 @@ function SidebarMenuButton({
       },
       props
     ),
-    render: !tooltip ? render : <TooltipTrigger asChild>{render}</TooltipTrigger>,
+    render,
     state: {
       slot: "sidebar-menu-button",
       sidebar: "menu-button",
@@ -539,7 +539,9 @@ function SidebarMenuButton({
 
   return (
     <Tooltip>
-      {comp}
+      <TooltipTrigger asChild>
+        {comp}
+      </TooltipTrigger>
       <TooltipContent
         side="right"
         align="center"
